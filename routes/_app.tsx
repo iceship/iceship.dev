2b23ae8369fd50665d0b64533d8ad23f55534ecf
Iceship <iceship@gmail.com>
2026-09-07 17:37:21 +0900

    feat: update favicon and logo SVG for improved visuals and responsiveness

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,8 +12,17 @@ export default define.page(function App({ Component }) {
         <title>iceship.dev</title>
         <meta name="description" content="iceship의 미니멀 개인 블로그" />
         <meta name="color-scheme" content="light dark" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link
+          rel="icon"
+          href="/favicon.ico?v=iceship-1"
+          sizes="16x16 32x32 48x48"
+        />
+        <link
+          rel="icon"
+          href="/logo.svg?v=iceship-1"
+          type="image/svg+xml"
+          sizes="any"
+        />
         <link
           rel="alternate"
           type="application/rss+xml"
